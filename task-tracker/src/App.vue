1506bc6e0c5c636b5edd2bcdc950647135cb04b5
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-08-12 14:26:50 +0200

    Create footer component

--- task-tracker/src/App.vue
+++ task-tracker/src/App.vue
@@ -8,11 +8,13 @@
       <AddTask @add-task="addTask"/>
     </div>
     <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks"/>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 import Tasks from './components/Tasks.vue';
 import AddTask from './components/AddTask.vue';
 
@@ -22,6 +24,7 @@ export default {
   name: 'App',
   components: {
     Header,
+    Footer,
     Tasks,
     AddTask,
   },
